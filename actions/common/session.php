f0bfa627796549a5128d938c680435199394fa44
Sandro Magalhães <sandro1995@gmail.com>
2017-10-30 23:50:37 +0000

    Deleted unnecessary code

--- actions/common/session.php
+++ actions/common/session.php
@@ -1,17 +1,6 @@
 <?php
-
-include("../../database/db_connect.php");
-session_start();
-
-connect_db();
-
-$user_session_check = $_SESSION["user_id"];
-$query = "SELECT username FROM utilizador WHERE username = '$user_session_check'";
-$result = pg_exec($conn, $query);
-
-$login_session = $result['username'];
-    if (!session_id()){
-        session_start();
+    session_start();
+    if (!isset($_SESSION["user_id"])){
         $_SESSION["user_id"] = NULL;
     }
 ?>
