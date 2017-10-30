b2077975132bd5c88716486176d4a4d8ed585d9c
MiniMiudo <mmtb4ever@gmail.com>
2017-10-30 11:33:31 +0000

    Update logout.php

--- actions/user/logout.php
+++ actions/user/logout.php
@@ -3,6 +3,6 @@
 session_start();
 session_destroy();
 
-echo 'Logout efetuado com sucesso. <a href="../../pages/films/home.php">Voltar à página de inicial</a>';
+echo 'Logout efetuado com sucesso. <a href="../../pages/films/home.php">Voltar à página inicial</a>';
 
 ?>
