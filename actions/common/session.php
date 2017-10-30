c249167720a43738d108b9bd9763d015d215ed87
MiniMiudo <mmtb4ever@gmail.com>
2017-10-30 11:50:54 +0000

    Update session.php

--- actions/common/session.php
+++ actions/common/session.php
@@ -1,4 +1,23 @@
 <?php
+
+session_start();
+
+$conn = pg_connect("host=db.fe.up.pt dbname=siem1742 user=siem1742 password=LrYaFMWy");
+if(!conn) {
+	print "Não foi possível estabelecer a ligação à BD";
+	exit;
+}
+
+$query = "set schema 'ssmovies';";
+pg_exec($conn, $query);
+
+$user_session_check = $_SESSION['login_user'];
+
+$query = "SELECT username FROM utilizador WHERE username = '$user_session_check'";
+$result = pg_exec($conn, $query);
+
+$login_session = $result['username'];
+
     if (!session_id()){
         session_start();
         $_SESSION["user_id"] = NULL;
