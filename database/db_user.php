fab488b45c0bc21204cbde6843da396f3631d81a
MiniMiudo <mmtb4ever@gmail.com>
2017-10-31 12:01:26 +0000

    Update db_user.php

--- database/db_user.php
+++ database/db_user.php
@@ -30,20 +30,20 @@
     }
 
     function new_user_db ($name, $email, $username, $password, $telephone, $nif, $address, $postcode, $localidade) {
-		global $conn;
+	global $conn;
 		 
-		$query = "SELECT * FROM utilizador";
-		$result = pg_exec ($conn, $query);
-		$numrows = pg_numrows($result);
+	$query = "SELECT * FROM utilizador";
+	$result = pg_exec ($conn, $query);
+	$numrows = pg_numrows($result);
 		 
-		$id = $numrows+1;
+	$id = $numrows+1;
 		 
 		 
-	    $query2 = "INSERT INTO utilizador (id, username, nome, email, password, telefone, nif, morada, codigo_postal, localidade, admin)
-				   VALUES ('$id', '$username', '$name', '$email', '$password', '$telephone', '$nif', '$address', '$postcode', '$localidade', 'FALSE')"; 
+	$query2 = "INSERT INTO utilizador (id, username, nome, email, password, telefone, nif, morada, codigo_postal, localidade, admin)
+		   VALUES ('$id', '$username', '$name', '$email', '$password', '$telephone', '$nif', '$address', '$postcode', '$localidade', 'FALSE')"; 
 				   
-		pg_exec ($conn, $query2);
+	pg_exec ($conn, $query2);
 		 
-		return header("Location: ../../pages/films/home.php");
+	return header("Location: ../../pages/films/home.php");
 	}
 ?>
