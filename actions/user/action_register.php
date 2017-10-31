8ad30716ec0080a8367619407655501e2d5479c5
MiniMiudo <mmtb4ever@gmail.com>
2017-10-31 11:50:32 +0000

    Update action_register.php

--- actions/user/action_register.php
+++ actions/user/action_register.php
@@ -1,14 +1,26 @@
 <?php
-
-    /* Recebe a variáveis por POST e cria as diferentes strings */
-    $name = $_POST["first_name"] . " " . $_POST["last_name"];
-    $email = $_POST["e_mail"];
+    
+	$name = $_POST["first_name"] . " " . $_POST["last_name"];
+    $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
     $telephone = $_POST["telephone"];
     $nif = $_POST["nif"];
-    $address = $_POST["addroess"];
-    $postcode = $_POST["postcod1"] . "-" . $_POST["postcode2"];
+    $address = $_POST["address"];
+    $postcode = $_POST["post-code1"] . "-" . $_POST["post-code2"];
     $localidade = $_POST["localidade"];
-
+	
+	include_once ("../../database/db_connect.php");
+    include_once ("../../database/db_user.php");
+	
+	global $conn;
+	
+	if (!isset($conn)){
+            connect_db();
+        }
+	
+	if (isset($_POST["registar"])){
+		new_user_db($name, $email, $username, $password, $telephone, $nif, $address, $postcode, $localidade);
+	}
+	
 ?>
