ad740320fe812bcb03dc1133ec51bb80c89a085d
MiniMiudo <mmtb4ever@gmail.com>
2017-10-31 11:53:44 +0000

    Update db_user.php

--- database/db_user.php
+++ database/db_user.php
@@ -28,4 +28,22 @@
         else
             return NULL;
     }
+
+    function new_user_db ($name, $email, $username, $password, $telephone, $nif, $address, $postcode, $localidade) {
+		global $conn;
+		 
+		$query = "SELECT * FROM utilizador";
+		$result = pg_exec ($conn, $query);
+		$numrows = pg_numrows($result);
+		 
+		$id = $numrows+1;
+		 
+		 
+	    $query2 = "INSERT INTO utilizador (id, username, nome, email, password, telefone, nif, morada, codigo_postal, localidade, admin)
+				   VALUES ('$id', '$username', '$name', '$email', '$password', '$telephone', '$nif', '$address', '$postcode', '$localidade', 'FALSE')"; 
+				   
+		pg_exec ($conn, $query2);
+		 
+		return header("Location: ../../pages/films/home.php");
+	}
 ?>
