dcbafc9ea0e196a66692920d04c4deed43c99b6b
Sandro Magalhães <sandro1995@gmail.com>
2017-11-01 19:34:40 +0000

    Adição dos dados padrão de inicio de sessão à página inicial

--- pages/films/home.php
+++ pages/films/home.php
@@ -24,14 +24,22 @@
         <section class="left-size">
             <h1 >Últimos Filmes</h1>
             <div class="content">
-                Lorem Ipsum
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In arcu ipsum, feugiat ac erat ac, aliquam hendrerit ex. Maecenas nisl tortor, dictum et volutpat id, suscipit vitae est. Vestibulum in interdum mi. Morbi viverra nibh in tellus suscipit efficitur ac in enim. Vivamus ultrices volutpat tristique. Phasellus et nunc vehicula, imperdiet nisl quis, varius sem. Praesent dictum pretium imperdiet. Phasellus eu convallis tortor. Aliquam fringilla ipsum magna, in finibus augue mollis eget. Quisque venenatis augue eget purus euismod egestas. Duis dignissim molestie augue, non dictum nisl lacinia at. Donec odio augue, facilisis nec massa eu, interdum tincidunt neque. Donec ac leo scelerisque, consequat felis vel, volutpat mi. Vivamus nulla ante, dictum eu eros nec, efficitur sollicitudin tortor. Donec nunc neque, tempus id interdum eu, pretium quis ante. Donec dictum laoreet velit efficitur blandit.
             </div>
         </section>
 
         <section class="right-size">
             <h1 >Informações de login</h1>
             <div class="content">
+                <p style="font-weight: bolder; font-variant: small-caps"> Conta de Utilizador </p>
+                <p><b>Username:</b> user </p>
+                <p><b>Password:</b> user </p>
 
+                <br />
+
+                <p style="font-weight: bolder; font-variant: small-caps"> Conta de Administrador </p>
+                <p><b>Username:</b> admin </p>
+                <p><b>Password:</b> admin </p>
             </div>
         </section>
     </div>
