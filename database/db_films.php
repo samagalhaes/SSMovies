803dd89cd277cc1120856257622f6ff4f28ac4e7
MiniMiudo <mmtb4ever@gmail.com>
2017-11-03 00:36:19 +0000

    Update db_films.php

--- database/db_films.php
+++ database/db_films.php
@@ -95,4 +95,14 @@
 
         return pg_exec($conn, $query);
     }
+
+function filter_films_db($ano, $genero1, $genero2, $genero3, $genero4, $genero5, $genero6, $genero7, $genero8, $genero9, $genero10, $genero11, $genero12, $genero13, $genero14, $genero15, $genero16, $genero17, $genero18, $genero19, $genero20, $genero21, $cl_etar) {
+		global $conn;
+		$query = "SELECT *
+			     FROM filme
+				 WHERE ano = $ano
+				 AND (genero = $genero1 OR genero = $genero2 OR genero = $genero3  OR genero = $genero4  OR genero = $genero5  OR genero = $genero6  OR genero = $genero7  OR genero = $genero8  OR genero = $genero9  OR genero = $genero10  OR genero = $genero11  OR genero = $genero12  OR genero = $genero13  OR genero = $genero14  OR genero = $genero15  OR genero = $genero16  OR genero = $genero17  OR genero = $genero18  OR genero = $genero19  OR genero = $genero20  OR genero = $genero21)
+				 AND classificacao_etaria = $cl_etar";
+		return pg_exec($conn, $query);
+	}
 ?>
