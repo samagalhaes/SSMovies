55aa6c446eca11c04842d7fa4f6a7afd52dfa2c3
Sandro Magalhães <sandro1995@gmail.com>
2017-11-12 12:53:29 +0000

    Adicionado o BASE_DIR e o BASE_URL

--- actions/common/session.php
+++ actions/common/session.php
@@ -2,6 +2,9 @@
     include_once("../../database/db_user.php");
     include_once("../../database/db_connect.php");
 
+    $BASE_DIR = "/usr/users2/mieec2013/up201304932/public_html/trabalhosSiem/TrabalhoPHP/";
+    $BASE_URL = "https://gnomo.fe.up.pt/~up201304932/trabalhosSiem/TrabalhoPHP/";
+
     /* Permite bloquear páginas com acesso restrito a utilizadores com login efectuado */
     function check_session ($path) {
         if ($_SESSION["user_id"] == NULL){
