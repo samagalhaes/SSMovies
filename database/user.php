2d4fe3a31ac9df3e504321dc91c66b7f40703108
Sandro Magalhães <sandro1995@gmail.com>
2017-12-24 00:09:25 +0000

    Created register action

--- database/user.php
+++ database/user.php
@@ -1,16 +1,25 @@
 <?php
+  function isLoginCorrect($username)
+  {
+    global $conn;
 
-	function isLoginCorrect ($username){
-		global $conn;
+    $options = ['cost' => 12];
 
-		$options = ['cost' => 12];
+    $stmt = $conn->prepare('SELECT *
+						FROM utilizador
+						WHERE username=?');
+    $stmt->execute(array($username));
 
-		$stmt = $conn->prepare('SELECT *
-														FROM utilizador
-														WHERE username=?');
-		$stmt->execute(array($username));
+    return $stmt->fetch();
+  }
 
-		return $stmt->fetch();
-	}
+  function createUser($name, $email, $username, $password, $telephone, $nif, $address, $postcode, $locality)
+  {
+    global $conn;
 
+		$stmt = $conn->prepare("INSERT INTO utilizador (username, nome, email, password, telefone, nif, morada, codigo_postal, localidade)
+														VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+		$stmt->execute(array($username, $name, $email, $password, $telephone, $nif, $address, $postcode, $locality));
+  }
 ?>
