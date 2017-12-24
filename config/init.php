d0dd7e60b09b7d8c6c0080c8219be509220e6a39
Sandro Magalhães <sandro1995@gmail.com>
2017-12-24 00:10:32 +0000

    Corrected success messages variable

--- config/init.php
+++ config/init.php
@@ -1,9 +1,13 @@
 <?php
-  $BASE_DIR = "/usr/users2/mieec2013/up201304932/public_html/trabalhosSiem/TrabalhoPHP-2/";
-  $BASE_URL = "https://gnomo.fe.up.pt/~up201304932/trabalhosSiem/TrabalhoPHP-2/";
+  session_start();
+
+  //$BASE_DIR = "/usr/users2/mieec2013/up201304932/public_html/trabalhosSiem/TrabalhoPHP-2/";
+  //$BASE_URL = "https://gnomo.fe.up.pt/~up201304932/trabalhosSiem/TrabalhoPHP-2/";
+  $BASE_DIR = "C:\www\TrabalhoPHP-2/";
+  $BASE_URL = "http://localhost/TrabalhoPHP-2/";
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
-	include_once($BASE_DIR . 'database/menus.php');
+  include_once($BASE_DIR . 'database/menus.php');
 
   /* Database connection */
   $conn = new PDO('pgsql:host=db.fe.up.pt;dbname=siem1742', 'siem1742', 'LrYaFMWy');
@@ -12,14 +16,23 @@
   $stmt = $conn->prepare("SET SCHEMA 'ssmovies'");
   $stmt->execute();
 
-	$smarty = new Smarty;
+  $smarty = new Smarty;
 
-	$smarty->template_dir = $BASE_DIR . 'templates/';
-	$smarty->compile_dir = $BASE_DIR . 'templates_c/';
+  $smarty->template_dir = $BASE_DIR . 'templates/';
+  $smarty->compile_dir = $BASE_DIR . 'templates_c/';
 
-	$smarty->assign ('BASE_URL', $BASE_URL);
+  $smarty->assign('BASE_URL', $BASE_URL);
 
-	/* Query main menu */
+  /* Success and error messages */
+  if (isset($_SESSION["error_messages"])) {
+      $smarty->assign('ERROR_MESSAGES', $_SESSION["error_messages"]);
+      unset($_SESSION["error_messages"]);
+  }
+  if (isset($_SESSION["success_messages"])) {
+      $smarty->assign('SUCCESS_MESSAGES', $_SESSION["success_messages"]);
+      unset($_SESSION["success_messages"]);
+  }
+
+    /* Query main menu */
   $mainMenu = listMenuItens(0);
-	$smarty->assign ('mainMenu', $mainMenu);
-?>
+  $smarty->assign('mainMenu', $mainMenu);
