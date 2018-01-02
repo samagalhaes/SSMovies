401b9e95ab403e6986c522028d79e82edc312976
MiniMiudo <mmtb4ever@gmail.com>
2018-01-02 02:34:55 +0000

    Update list_encomendas.php

--- pages/encomendas/list_encomendas.php
+++ pages/encomendas/list_encomendas.php
@@ -3,7 +3,7 @@
   include_once($BASE_DIR.'database/encomendas.php');
   
   if(!isset($_SESSION['user'])) {
-		header('Location: ../../pages/films/home.php');
+		header('Location: '.$BASE_URL.'pages/films/home.php');
 		exit();
 	}
   
