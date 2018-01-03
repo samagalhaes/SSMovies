506831bf862cf049d0a5eddd41eed894325e7896
MiniMiudo <mmtb4ever@gmail.com>
2018-01-03 17:41:22 +0000

    Restrição do acesso através do URL a certas páginas

--- database/films.php
+++ database/films.php
@@ -80,4 +80,17 @@
     }
     return $films;
    }
+
+   function checkId($id) {
+    global $conn;
+
+    $stmt = $conn->prepare(
+      "SELECT *
+       FROM filme
+       WHERE id = ?");
+
+    $stmt->execute(array($id));
+
+    return $stmt->rowCount($stmt);
+   }
 ?>
